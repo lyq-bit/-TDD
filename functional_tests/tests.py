c7c57eec235b1be1b53212989597a7b8fa9d0bca
lyq-bit <729317332@qq.com>
2020-06-26 11:28:26 +0800

    1

---
+++ functional_tests/tests.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from django.test import LiveServerTestCase
+import time
+
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
+    def test_can_start_a_list_for_one_user(self):
+        self.browser.get(self.live_server_url)
+        
+        self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        inputbox.send_keys('Buy peacock feathers' )
+        
+
+        inputbox.send_keys(Keys.ENTER)
+        
+        
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('aaa')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('2:aaa')
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+        
+        
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
+    
+
+    def test_multiple_users_can_start_lists_at_differtent_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers' )
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('11111', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('2222' )
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:2222')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(edith_list_url,francis_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('2222', page_text)
+
+        
+
+
+
